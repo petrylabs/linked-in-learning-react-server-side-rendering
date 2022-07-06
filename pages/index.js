3e9eeef007e97c475034ca22cdc806761fb010de
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-07-06 14:23:26 -0300

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import styles from './index.module.css';
 import Card from './Card';
-import data from './API/data.json';
+import { initStore, initialCards, addItem } from '../store';
 
-export default class Index extends React.Component {
-    static async getInitialProps () {
-        return {
-            cards: data
-        }
+class Index extends React.Component {
+    static async getInitialProps ({ store }) {
+        return store.dispatch(initialCards());
     }
+
     render () {
         return (
             <div className={styles.app}>
                 <header className={styles.header}>
-                    <img 
-                        src="/logo.png" 
-                        alt="logo" 
-                        className={styles.logo} 
+                    <img src="/logo.png"
+                        className={styles.logo} alt="logo"
                     />
-                    <div className={styles.grid}>
-                        {this.props.cards.map(card => (
-                            <Card key={card.id} />
-                        ))}
-                    </div>
                 </header>
+                <div className={styles.grid}>
+                   {
+                       this.props.cards.map((card) => (
+                           <Card key={card.id} />
+                       ))
+                   }
+                </div>
+                {/* <button onClick={() => dispatch(addItem())}></button> */}
             </div>
         )
     }
-}
+};
+
+export default initStore.withRedux(Index);
