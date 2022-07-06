831e3624656c7d821e8eade138447d14301a0f8f
Michael Petry <mpetry@LAPTOP-D6RBF22U>
2022-07-06 10:30:46 -0300

    Add files for demonstrating how Next.js works
    Copy and paste files from exercise files and pasted into pages folder to generate static page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
-export default () => (
-    <p>Hello World!</p>
-);
+import React from 'react';
+import styles from './index.module.css';
+import Card from './Card';
+import data from './API/data.json';
+
+export default class Index extends React.Component {
+    static async getInitialProps () {
+        return {
+            cards: data
+        }
+    }
+    render () {
+        return (
+            <div className={styles.app}>
+                <header className={styles.header}>
+                    <img 
+                        src="/logo.png" 
+                        alt="logo" 
+                        className={styles.logo} 
+                    />
+                    <div className={styles.grid}>
+                        {this.props.cards.map(card => (
+                            <Card key={card.id} />
+                        ))}
+                    </div>
+                </header>
+            </div>
+        )
+    }
+}
